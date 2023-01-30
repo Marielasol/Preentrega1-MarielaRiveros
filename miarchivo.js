74356f82f375fee68c47ffdc70c60960cab5bdb6
Marielasol <116029754+Marielasol@users.noreply.github.com>
2023-01-29 23:53:35 -0300

    Update miarchivo.js

--- miarchivo.js
+++ miarchivo.js
@@ -2,6 +2,24 @@
 let nombre = prompt("Ingrese su nombre para conocer los valores");
 alert ("Bienvenido " + nombre);
 
+let servicio = prompt("Elija el servicio a realizar: Combo novia, 15 años");
+while (servicio != "continuar") {
+    switch servicio {
+        case "combo novia":
+            let precio = cupon(10000, 10);
+            alert("El valor del servicio Combo Novia es de $" + precio);
+            break;
+        case "15 años":
+            let precio = cupon(8000, 10);
+            alert("El valor del servicio Combo Novia es de $" + precio);
+            break;
+    }
+    servicio = prompt("Elija el servicio a realizar: Combo novia, 15 años")
+}
+function cupon(precioComboNovia, descuento) {
+    let resultado = precioComboNovia - (precioComboNovia * descuento / 100)
+    return resultado;
+}
 
 
 
